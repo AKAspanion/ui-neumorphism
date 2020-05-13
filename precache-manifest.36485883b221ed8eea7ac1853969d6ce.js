77113c3aae2bca48cc75e910b7d7039fe4c9835a
Ankit <panditankit1995@gmail.com>
2020-05-13 14:49:16 +0530

    Updates

---
+++ precache-manifest.36485883b221ed8eea7ac1853969d6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71cfdead1b4c1d9542e1d219739f2c4",
+    "url": "/ui-neumorphism/index.html"
+  },
+  {
+    "revision": "d3cf6b8c0b89e74b15d0",
+    "url": "/ui-neumorphism/static/css/main.6eed0d32.chunk.css"
+  },
+  {
+    "revision": "d7309b8bd940972843c0",
+    "url": "/ui-neumorphism/static/js/2.186f842f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ui-neumorphism/static/js/2.186f842f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cf6b8c0b89e74b15d0",
+    "url": "/ui-neumorphism/static/js/main.8efbdb46.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/ui-neumorphism/static/js/main.8efbdb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c81a19ae06151500a64",
+    "url": "/ui-neumorphism/static/js/runtime-main.a04be4ad.js"
+  }
+]);
