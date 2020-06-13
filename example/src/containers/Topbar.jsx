400f7aec9dc570878c3c11c3f6b0f654d826c363
Ankit Kumar <panditankit1995@gmail.com>
2020-06-14 01:59:02 +0530

    pakage published to v1.0.4

--- example/src/containers/Topbar.jsx
+++ example/src/containers/Topbar.jsx
@@ -1,12 +1,23 @@
 import React, { createElement } from 'react'
 import Icon from '@mdi/react'
-import { mdiMenu, mdiGithub, mdiLightbulbOutline, mdiLightbulb } from '@mdi/js'
+import {
+  mdiNpm,
+  mdiMenu,
+  mdiGithub,
+  mdiLightbulb,
+  mdiLightbulbOutline
+} from '@mdi/js'
 
 import { H2, H6, Card, IconButton, ToggleButton } from 'ui-neumorphism'
 
+const npmUrl = 'https://www.npmjs.com/package/ui-neumorphism'
 const githubUrl = 'https://github.com/AKAspanion/ui-neumorphism'
 
 class Topbar extends React.Component {
+  open(url) {
+    window.open(url, '_blank')
+  }
+
   render() {
     const { dark, onClick, onMenuClick, size } = this.props
     const isSmall = size === 'sm' || size === 'xs'
@@ -30,8 +41,14 @@ class Topbar extends React.Component {
         </Card>
         <Card flat className='d-flex align-center topbar-actions'>
           <IconButton
+            className='topbar-action mr-1'
+            onClick={() => this.open(npmUrl)}
+          >
+            <Icon path={mdiNpm} size={1.4} />
+          </IconButton>
+          <IconButton
             className='topbar-action'
-            onClick={() => window.open(githubUrl, '_blank')}
+            onClick={() => this.open(githubUrl)}
           >
             <Icon path={mdiGithub} size={1} />
           </IconButton>
