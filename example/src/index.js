2e3244e14245d7f1c69ba511e589903f643d1f29
Alfonso Guerra <huperniketes@gmail.com>
2023-01-12 13:17:44 -0500

    Updated the ReactDOM client code so the example conforms to React 18 usage.

--- example/src/index.js
+++ example/src/index.js
@@ -1,13 +1,15 @@
 import './index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
+const	container = document.getElementById('root');
+const	root = createRoot(container);
+
+root.render(
   <Router basename={'/ui-neumorphism'}>
     <App />
   </Router>,
-  document.getElementById('root')
 )
